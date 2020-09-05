47d044dcc80de2f61f01eb3465bf4475f549ad7f
vivomo <929992114@qq.com>
2020-09-06 00:02:11 +0800

    层级样式

--- src/component/tree/TreeNode.tsx
+++ src/component/tree/TreeNode.tsx
@@ -10,39 +10,43 @@ class TreeNode extends React.Component<TreeNodeProps, any> {
         let {data} = this.props;
 
         return (
-            <ExpandedKeyContext.Consumer>
-                {
-                    // @ts-ignore
-                    ({keys, cb}) => data.map((item) => {
-                        let className, isOpenFolder = false;
-                        if (item.isLeaf) {
-                            className = 'tree-file'
-                        } else {
-                            // @ts-ignore
-                            isOpenFolder = keys.includes(item.key);
-                            className = isOpenFolder ? 'tree-folder-open' : 'tree-folder-close';
-                        }
-                        return (
-                            <div key={item.key}>
-                                <div className="tree-title-wrap" onClick={() => {
-                                    if (!item.isLeaf) {
-                                        cb(item.key);
+            <div className="tree-wrap">
+
+                <ExpandedKeyContext.Consumer>
+                    {
+                        // @ts-ignore
+                        ({keys, cb}) => data.map((item) => {
+                            let className, isOpenFolder = false;
+                            if (item.isLeaf) {
+                                className = 'tree-file'
+                            } else {
+                                // @ts-ignore
+                                isOpenFolder = keys.includes(item.key);
+                                className = isOpenFolder ? 'tree-folder-open' : 'tree-folder-close';
+                            }
+                            return (
+                                <div key={item.key}>
+                                    <div className="tree-title-wrap" onClick={() => {
+                                        if (!item.isLeaf) {
+                                            cb(item.key);
+                                        }
+                                    }}>
+                                        <span className={`tree-item-icon ${className}`}/>
+                                        <span>{item.title}</span>
+                                    </div>
+                                    {
+                                        isOpenFolder ?
+                                            <TreeNode data={item.children}/>
+                                            :
+                                            null
                                     }
-                                }}>
-                                    <span className={`tree-item-icon ${className}`}/>
-                                    <span>{item.title}</span>
                                 </div>
-                                {
-                                    isOpenFolder ?
-                                        <TreeNode data={item.children}/>
-                                        :
-                                        null
-                                }
-                            </div>
-                        )
-                    })
-                }
-            </ExpandedKeyContext.Consumer>
+                            )
+                        })
+                    }
+                </ExpandedKeyContext.Consumer>
+            </div>
+
         );
     }
 }
